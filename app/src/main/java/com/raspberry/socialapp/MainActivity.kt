7aa4d828f7a025f511d16a7702993e35a7200f8e
gallet <gallet@MacBook-Pro-de-gallet.local>
2017-07-30 12:05:57 +0200

    dagger

--- app/src/main/java/com/raspberry/socialapp/MainActivity.kt
+++ app/src/main/java/com/raspberry/socialapp/MainActivity.kt
@@ -32,7 +32,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
 
         val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
         drawer_layout.setDrawerListener(toggle)
         toggle.syncState()
 
