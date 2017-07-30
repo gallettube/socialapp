304c58d2b24eb071a2cfbb27aa71bf38fb922b68
gallet <gallet@MacBook-Pro-de-gallet.local>
2017-07-30 03:39:51 +0200

    kotlin synteticos anotaciones

---
+++ app/src/main/java/com/raspberry/socialapp/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.raspberry.socialapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private var callbackManager: CallbackManager? = null
+    private var firebaseAuthListener: FirebaseAuth.AuthStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        callbackManager = CallbackManager.Factory.create()
+
+        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Toast.makeText(applicationContext, R.string.com_facebook_loginview_cancel_action, Toast.LENGTH_SHORT)
+            }
+
+            override fun onError(error: FacebookException) {
+                Toast.makeText(applicationContext, "Error login", Toast.LENGTH_SHORT)
+            }
+        })
+
+
+        firebaseAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                goMainScreen()
+            }
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager!!.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
+        FirebaseAuth.getInstance()!!.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (!task.isSuccessful) {
+
+                Toast.makeText(applicationContext, "Error connecting firebase login", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun goMainScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(
+                Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance()!!.addAuthStateListener(firebaseAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance()!!.removeAuthStateListener(firebaseAuthListener!!)
+    }
+
+
+}
--- app/src/main/java/com/raspberry/socialapp/MainActivity.kt
+++ app/src/main/java/com/raspberry/socialapp/MainActivity.kt
@@ -2,19 +2,17 @@ package com.raspberry.socialapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import com.facebook.AccessToken
 import com.facebook.login.LoginManager
-
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -26,24 +24,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             goLoginScreen();
         }
 
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
         val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.setDrawerListener(toggle)
         toggle.syncState()
 
 
-        val navigationView = findViewById(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     private fun goLoginScreen() {
@@ -54,9 +48,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
     override fun onBackPressed() {
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
@@ -74,37 +67,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
-
         if (id == R.id.action_settings) {
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         val id = item.itemId
-
         if (id == R.id.nav_camera) {
-            // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
         } else if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
-
         } else if (id == R.id.nav_send) {
-
         } else if (id == R.id.log_out) {
             LoginManager.getInstance().logOut();
             goLoginScreen();
         }
-
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        drawer.closeDrawer(GravityCompat.START)
+        drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
 }
